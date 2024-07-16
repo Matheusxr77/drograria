5a97f1bbfc58b4a074658e16c13cd47c9d283baf
Matheusxr77 <suehtamxr77@gmail.com>
2024-07-16 00:17:09 -0300

    update repository

--- src/main/java/br/org/drogaria/repository/RemedioRepository.java
+++ src/main/java/br/org/drogaria/repository/RemedioRepository.java
@@ -2,7 +2,9 @@ package br.org.drogaria.repository;
 
 import br.org.drogaria.entity.Remedio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RemedioRepository extends JpaRepository<Remedio, Long> {
-    
+
 }
