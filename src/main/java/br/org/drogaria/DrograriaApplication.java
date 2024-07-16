3ee5f060d287d07a3151957d917113bd272acf13
Matheusxr77 <suehtamxr77@gmail.com>
2024-07-15 23:50:04 -0300

    Initial commit

---
+++ src/main/java/br/org/drogaria/DrograriaApplication.java
@@ -0,0 +1,13 @@
+package br.org.drogaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrograriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrograriaApplication.class, args);
+	}
+
+}
